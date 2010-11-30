1428c047d9aba0c6d9cf0b52c1f3cdba0c1c8c2f
Raphael Schober <raphael.schober@rubicon.eu>
2010-11-30 11:43:22 +0000

    added several methods for trackedassembly lookup and handling of tracked assemblies.

--- AssemblyTransformer/AssemblyTransformer/AssemblyTransformer.UnitTests/AssemblyTrackerTest.cs
+++ AssemblyTransformer/AssemblyTransformer/AssemblyTransformer.UnitTests/AssemblyTrackerTest.cs
@@ -49,7 +49,53 @@ namespace AssemblyTransformer.UnitTests
       var result = tracker.GetAssemblyByReference (name);
 
       Assert.That (result, Is.SameAs (assembly));
+    }
 
+    [Test]
+    public void GetAssemblyByReference_NoMatchFound ()
+    {
+      var name = AssemblyNameReferenceObjectMother.CreateAssemblyNameReferenceWithCulture ("TestCase1", "de");
+
+      AssemblyDefinition assembly = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1");
+      AssemblyDefinition assembly2 = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1", "en");
+      AssemblyTracker tracker = new AssemblyTracker (new[] { assembly, assembly2 });
+
+      var result = tracker.GetAssemblyByReference (name);
+
+      Assert.That (result, Is.Null);
+    }
+
+    [Test]
+    public void MarkModified ()
+    {
+      AssemblyDefinition assembly = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1");
+      AssemblyDefinition assembly2 = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1", "en");
+      AssemblyTracker tracker = new AssemblyTracker (new[] { assembly, assembly2 });
+
+      tracker.MarkModified (assembly);
+
+      Assert.That (tracker.IsModified (assembly), Is.True);
+      Assert.That (tracker.IsModified (assembly2), Is.False);
+    }
+
+    [Test]
+    public void MarkModified_NotFound()
+    {
+      AssemblyDefinition assembly = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1");
+      AssemblyDefinition assembly2 = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1", "en");
+      AssemblyTracker tracker = new AssemblyTracker (new[] { assembly });
+
+      Assert.Throws<ArgumentException> (() => tracker.MarkModified (assembly2));
+    }
+
+    [Test]
+    public void IsModified_NotFound ()
+    {
+      AssemblyDefinition assembly = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1");
+      AssemblyDefinition assembly2 = AssemblyDefinitionObjectMother.CreateAssemblyDefinition ("TestCase1", "en");
+      AssemblyTracker tracker = new AssemblyTracker (new[] { assembly });
+
+      Assert.Throws<ArgumentException> (() => tracker.IsModified (assembly2));
     }
   }
 }
--- AssemblyTransformer/AssemblyTransformer/AssemblyTransformer/Implementation/AssemblyTracker.cs
+++ AssemblyTransformer/AssemblyTransformer/AssemblyTransformer/Implementation/AssemblyTracker.cs
@@ -11,21 +11,45 @@ namespace AssemblyTransformer
 {
   public class AssemblyTracker : IAssemblyTracker
   {
-    private readonly IEnumerable<AssemblyDefinition> _assemblyDefinitions;
+    private readonly IDictionary<AssemblyDefinition, TrackedAssembly> _trackedAssemblies;
 
     public AssemblyTracker (IEnumerable<AssemblyDefinition> assemblyDefinitions)
     {
-      _assemblyDefinitions = assemblyDefinitions;
+      _trackedAssemblies = assemblyDefinitions
+        .Select (asm => new TrackedAssembly (asm))
+        .ToDictionary (trackedAsm => trackedAsm.AssemblyDefinition);
     }
 
     public IEnumerable<AssemblyDefinition> GetAssemblies ()
     {
-      return _assemblyDefinitions;
+      return _trackedAssemblies.Keys;
     }
 
     public AssemblyDefinition GetAssemblyByReference (AssemblyNameReference referencedAssemblyName)
     {
-      return _assemblyDefinitions.Single (asm => referencedAssemblyName.MatchesDefinition (asm.Name));
+      var assemblyDefinitions = GetAssemblies();
+      return assemblyDefinitions.SingleOrDefault (asm => referencedAssemblyName.MatchesDefinition (asm.Name));
+    }
+
+    public void MarkModified (AssemblyDefinition assemblyDefinition)
+    {
+      var trackedAssembly = GetTrackedAssembly(assemblyDefinition);
+      trackedAssembly.MarkModified();
+    }
+
+    public bool IsModified (AssemblyDefinition assemblyDefinition)
+    {
+      var trackedAssembly = GetTrackedAssembly (assemblyDefinition);
+      return trackedAssembly.IsModified;
+    }
+
+    private TrackedAssembly GetTrackedAssembly (AssemblyDefinition assemblyDefinition)
+    {
+      TrackedAssembly result;
+      if (!_trackedAssemblies.TryGetValue (assemblyDefinition, out result))
+        throw new ArgumentException ("The assembly '{0}' is not a tracked assembly.", assemblyDefinition.FullName);
+      
+      return result;
     }
   }
 }
